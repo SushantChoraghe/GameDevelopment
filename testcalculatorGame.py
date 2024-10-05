0439fa41bae167c001c027dacb19fcd956327dbf
sushant <career.sushantchoraghe@gmail.com>
2024-10-05 05:53:47 +0200

    Asach tp

--- testcalculatorGame.py
+++ testcalculatorGame.py
@@ -1,7 +1,7 @@
 from calculatorGame import calculator
 
-a= int(input("Enter the value of a:"))
+a= int(input("Enter the value of a- "))
 
-b= int(input("Enter the value of b:"))
+b= int(input("Enter the value of b- "))
 
 print(calculator(a,b))
